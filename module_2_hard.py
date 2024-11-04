1ba03e72bf90bad5b8486b948dca4184fe879d16
getala72 <getala72@gmail.com>
2024-11-04 23:04:04 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -143,19 +143,64 @@
 
 Файл с кодом (module_2_hard.py) прикрепите к домашнему заданию или пришлите ссылку на ваш GitHub репозиторий с файлом решения.
 '''
-def generate_password(n):
-    pairs = []
-    for i in range(1, n):
-        for j in range(i + 1, n):
-            if n % (i + j) == 0:
-                pairs.append((i, j))
+import random
 
+
+num_list = []
+
+
+def list_create(start=3,end=20):
+    for i in range(start, end + 1):
+        num_list.append(i)
+    return num_list
+
+def left_num():
+    left = list_create()[random.randint(list_create()[0],len(list_create()))]
+    return left
+
+def spisok_par(n=20):
+
+    spisok = []
+    for i in range(n,0,-1):
+        para = []
+        if i >= 0 and n-i > 0:
+            #print(n-i,'+',i)
+            if (n-i) or i not in para:
+                para.append(n-i)
+                para.append(i)
+            if [para[1],para[0]] not in spisok:
+                spisok.append(para)
+
+        else:
+            continue
+    return spisok
+
+
+
+def algoritm(get_num):
+    right_list = []
+
+    for i in range(get_num-1,0,-1):
+        if get_num % i == 0:
+            if len(spisok_par(i)) > 0:
+                for p in spisok_par(i):
+                    right_list.append(p)
+    for i in spisok_par(get_num):
+        right_list.append(i)
+    for i in right_list:
+        if i[0] == i[1]:
+            right_list.remove(i)
+    right_list = sorted(right_list)
     result = ''
-    for pair in pairs:
-        result += str(pair[0]) + str(pair[1])
+    for x in right_list:
+        for y in x:
+            result += str(y)
+    print("Число слева:", get_num, "Пароль:", result)
 
-    return result
 
-n = int(input("Введите число n (от 3 до 20): "))
-result = generate_password(n)
-print(result)
+def result():
+    for i in list_create(3,20):
+        algoritm(i)
+
+
+result()
